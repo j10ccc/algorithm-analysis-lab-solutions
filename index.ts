a92bbdeb21e13d6d5e56f91297798be81d452f80
j10c <blyb1739@gmail.com>
2023-12-01 11:40:49 +0800

    feat(entry): measure run time

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import Assignment1Solution from "assignment1-solutions";
 import Assignment2Solution from "assignment2-solutions";
 import fs from "fs";
+import { performance } from "perf_hooks";
 
 const ALGORITHMS = {
   ...Assignment1Solution,
@@ -13,7 +14,10 @@ const input = process.argv[process.argv.indexOf("--input") + 1]
 
 const data: any = require(input);
 
+const startTime = performance.now();
 const result = ALGORITHMS[algorithm](data);
+const endTime = performance.now();
+console.log(chalk.blue("Run time:", endTime - startTime, "ms"));
 
 const output = `./out/${algorithm}/${algorithm}_${new Date().getTime()}.json`;
 
